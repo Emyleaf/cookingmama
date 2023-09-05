c678469f118e84372115e7d4965036384069bc4c
Emyleaf <emyleaf@gmail.com>
2023-09-05 20:12:45 +0200

    v1.2f

--- js/riceps.js
+++ js/riceps.js
@@ -22,6 +22,17 @@ function popupPastapomodoro(){
     });
 }
 
+function popupCarbonara(){
+    Swal.fire({
+        title: '<p class="swalfont">Carbonara 🍝</p>',
+        html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><br>In corso...",
+        confirmButtonText: 'Capito!',
+        customClass: {
+            content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
+        },
+    });
+}
+
 //secondi
 
 function popupHamburger(){
@@ -53,6 +64,17 @@ function popupCheesecake(){
         title: '<p class="swalfont">Cheesecake 🍰</p>',
         html: "<img src='./img/icon.jpeg'></img><br><br><strong>INGREDIENTI</strong><br><strong>Per la base:</strong><br>250 g di biscotti tipo digestive<br>125 g di burro.<br><strong>Per il ripieno:</strong><br> 750 g di Philadelphia<br>170 ml di panna fresca<br>80 g di zucchero<br>8 g di gelatina alimentare in fogli<br><strong>Per la copertura:</strong><br>250 g di confettura di lamponi<br>qualche goccia di succo di limone<br>mezzo bicchiere di acqua<br><br><strong>PREPARAZIONE</strong><br><strong>Per la base:</strong> spaccare i biscotti fino ad ottenere una polvere fine e mescolare col burro fuso. Livellare nello stampo e mettere in frigo in attesa del resto.<br><br><strong>Per il ripieno:</strong> mettere in ammollo la gelatina in acqua fredda per il tempo indicato sulla confezione, circa 10 minuti.<br>In una ciotola montare il formaggio fresco con lo zucchero a velo e 150 g di panna, fino a ottenere un composto omogeneo. Porre la panna restante in un pentolino e fatela scaldare.<br><br> Unire la gelatina ben strizzata alla panna calda e mescolare fino a quando si sarà sciolta. Far raffreddare e aggiungere il miscuglio al composto di formaggio fresco e mescolare.<br> Versare il tutto sulla base di biscotti, livellare e porre in frigorifero per un paio d'ore (più ci sta, più si solidifica per bene).<br><br><strong>Per la GANESH:</strong> scaldare la marmellata di lamponi in un pentolino con l'acqua e il succo di limone. Una volta tiepida, versarla sulla superficie del dolce. Porre nuovamente in frigorifero per un paio d'ore.",
         //html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><ol><li>Iniziate preparando la base per il cheesecake.</li><li>Tritate finemente i biscotti secchi di vostra scelta (solitamente si usano biscotti tipo digestive o oreo).</li><li>In una ciotola, mescolate i biscotti tritati con del burro fuso fino a ottenere una consistenza simile a sabbia umida.</li><li>Pressate questa miscela di biscotti sul fondo di una teglia a cerniera precedentemente foderata con carta forno. Assicuratevi di compattarla bene.</li><li>Mettete la base in frigorifero mentre preparate il ripieno.</li></ol><strong>Per il ripieno:</strong><ol><li>In una ciotola capiente, sbattete il formaggio cremoso (tipo Philadelphia) fino a renderlo liscio e cremoso.</li><li>Aggiungete lo zucchero a velo e continuate a mescolare fino a ottenere una consistenza omogenea.</li><li>Aggiungete la vaniglia e il succo di limone e mescolate bene.</li><li>In un'altra ciotola, montate la panna fresca fino a ottenere picchi morbidi.</li><li>Incorporate delicatamente la panna montata al composto di formaggio, mescolando con movimenti delicati dal basso verso l'alto per non smontare la panna.</li><li>Versate il ripieno sulla base di biscotti nella teglia.</li><li>Livellate la superficie con una spatola e coprite la teglia con pellicola trasparente.</li><li>Mettete il cheesecake in frigorifero e lasciatelo raffreddare per almeno 4 ore o preferibilmente durante la notte.</li></ol><strong>Per la guarnizione:</strong><ol><li>Prima di servire, decorate il cheesecake con frutti di bosco freschi o sciroppo di frutta, se desiderate.</li><li>Rimuovete con attenzione il cheesecake dalla teglia a cerniera e trasferitelo su un piatto da portata.</li><li>Tagliate a fette e gustate il vostro delizioso cheesecake freddo!</li></ol>",
+        confirmButtonText: 'Capito!',
+        customClass: {
+            content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
+        },
+    });
+}
+
+function popupTortaKinder(){
+    Swal.fire({
+        title: '<p class="swalfont">Torta Kinder 🎂</p>',
+        html: '<img src="./img/icon.jpeg"></img><br><br><strong>PREPARAZIONE</strong><br>In corso...<br><br>',
         confirmButtonText: 'Capito!',
         customClass: {
             content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
