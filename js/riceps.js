1488010497f6607ee3842d31499c7ab42cd96dde
Emyleaf <emyleaf@gmail.com>
2023-09-05 19:38:57 +0200

    v1.2frittata

--- js/riceps.js
+++ js/riceps.js
@@ -1,7 +1,7 @@
-function popupHamburger(){
+function popupCoff(){
     Swal.fire({
-        title: '<p class="swalfont">Hamburger con patatine</p>',
-        html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><br>Riscaldare una padella per gli hamburger e una per le patate.<br><br> Nella padella per hamburger mettere un po' di sale, in quella per le patate dell'olio vergine di oliva (poco, giusto per ricoprire la padella) per quelle arrosto, altrimenti olio di semi girasole (leggermente di più rispetto a quello di oliva, le patate si devono un pelo immergere) per quelle fritte.<br><br> <strong>Per le patate:</strong> mettere le patate in pentola a fuoco lento, facendo attenzione agli eventuali schizzi dell'olio caldo con l'acqua contenuta nelle patate surgelate (l'acqua nell'olio bollente fa DANNI!)<br><br><strong>Per la carne:</strong> mettere l'hamburger in padella a fuoco basso e girarlo ogni tanto per non farlo bruciare. ",
+        title: '<p class="swalfont">Caffè! ☕</p>',
+        html: "<img src='./img/icon.jpeg'></img><br><br><p class='swalfont'><strong>PREPARAZIONE</strong><br>Inserire acqua potabile nella parte sottostante della moka, SENZA superare la valvola.<br><br> Inserire il filtrino e mettere il caffè senza pressarlo, chiudere il tutto.<br><br>Mettere su un fornello a fiamma bassa e nel giro di pochi minuti avrai il tuo caffè!<br><br><strong>Per la pulizia:</strong> rimuovere tutto il caffè sotto acqua calda corrente. Usare solo spugnette, SENZA sapone per piatti!<br><br><iframe width='300' height='250' src='https://www.youtube.com/embed/yTMW9QWSvAE?si=cnmlA55oj372FWxe&amp;controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe></p>",
         confirmButtonText: 'Capito!',
         customClass: {
             content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
@@ -9,9 +9,11 @@ function popupHamburger(){
     });
 }
 
+//primi
+
 function popupPastapomodoro(){
     Swal.fire({
-        title: '<p class="swalfont">Pasta al pomodoro</p>',
+        title: '<p class="swalfont">Pasta al pomodoro 🍝</p>',
         html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><br>Riscaldare un pentolo d'acqua fino a ebollizione. Mettere un pugnetto di sale grosso.<br><br><strong>Per la pasta:</strong> immergere la pasta e abbassare il fuoco a lento. <br><br><strong>Per il pomodoro:</strong> mescolare il passato di pomodoro con un filo d'olio d'oliva e scaldare a fuoco lento. Girarlo spesso per non lasciarlo bruciare.",
         confirmButtonText: 'Capito!',
         customClass: {
@@ -20,11 +22,12 @@ function popupPastapomodoro(){
     });
 }
 
-function popupCheesecake(){
+//secondi
+
+function popupHamburger(){
     Swal.fire({
-        title: '<p class="swalfont">Cheesecake</p>',
-        html: "<img src='./img/icon.jpeg'></img><br><br><strong>INGREDIENTI</strong><br><strong>Per la base:</strong><br>250 g di biscotti tipo digestive<br>125 g di burro.<br><strong>Per il ripieno:</strong><br> 750 g di Philadelphia<br>170 ml di panna fresca<br>80 g di zucchero<br>8 g di gelatina alimentare in fogli<br><strong>Per la copertura:</strong><br>250 g di confettura di lamponi<br>qualche goccia di succo di limone<br>mezzo bicchiere di acqua<br><br><strong>PREPARAZIONE</strong><br><strong>Per la base:</strong> spaccare i biscotti fino ad ottenere una polvere fine e mescolare col burro fuso. Livellare nello stampo e mettere in frigo in attesa del resto.<br><br><strong>Per il ripieno:</strong> mettere in ammollo la gelatina in acqua fredda per il tempo indicato sulla confezione, circa 10 minuti.<br>In una ciotola montare il formaggio fresco con lo zucchero a velo e 150 g di panna, fino a ottenere un composto omogeneo. Porre la panna restante in un pentolino e fatela scaldare.<br><br> Unire la gelatina ben strizzata alla panna calda e mescolare fino a quando si sarà sciolta. Far raffreddare e aggiungere il miscuglio al composto di formaggio fresco e mescolare.<br> Versare il tutto sulla base di biscotti, livellare e porre in frigorifero per un paio d'ore (più ci sta, più si solidifica per bene).<br><br><strong>Per la GANESH:</strong> scaldare la marmellata di lamponi in un pentolino con l'acqua e il succo di limone. Una volta tiepida, versarla sulla superficie del dolce. Porre nuovamente in frigorifero per un paio d'ore.",
-        //html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><ol><li>Iniziate preparando la base per il cheesecake.</li><li>Tritate finemente i biscotti secchi di vostra scelta (solitamente si usano biscotti tipo digestive o oreo).</li><li>In una ciotola, mescolate i biscotti tritati con del burro fuso fino a ottenere una consistenza simile a sabbia umida.</li><li>Pressate questa miscela di biscotti sul fondo di una teglia a cerniera precedentemente foderata con carta forno. Assicuratevi di compattarla bene.</li><li>Mettete la base in frigorifero mentre preparate il ripieno.</li></ol><strong>Per il ripieno:</strong><ol><li>In una ciotola capiente, sbattete il formaggio cremoso (tipo Philadelphia) fino a renderlo liscio e cremoso.</li><li>Aggiungete lo zucchero a velo e continuate a mescolare fino a ottenere una consistenza omogenea.</li><li>Aggiungete la vaniglia e il succo di limone e mescolate bene.</li><li>In un'altra ciotola, montate la panna fresca fino a ottenere picchi morbidi.</li><li>Incorporate delicatamente la panna montata al composto di formaggio, mescolando con movimenti delicati dal basso verso l'alto per non smontare la panna.</li><li>Versate il ripieno sulla base di biscotti nella teglia.</li><li>Livellate la superficie con una spatola e coprite la teglia con pellicola trasparente.</li><li>Mettete il cheesecake in frigorifero e lasciatelo raffreddare per almeno 4 ore o preferibilmente durante la notte.</li></ol><strong>Per la guarnizione:</strong><ol><li>Prima di servire, decorate il cheesecake con frutti di bosco freschi o sciroppo di frutta, se desiderate.</li><li>Rimuovete con attenzione il cheesecake dalla teglia a cerniera e trasferitelo su un piatto da portata.</li><li>Tagliate a fette e gustate il vostro delizioso cheesecake freddo!</li></ol>",
+        title: '<p class="swalfont">Hamburger con patatine 🍔🍟</p>',
+        html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><br>Riscaldare una padella per gli hamburger e una per le patate.<br><br> Nella padella per hamburger mettere un po' di sale, in quella per le patate dell'olio vergine di oliva (poco, giusto per ricoprire la padella) per quelle arrosto, altrimenti olio di semi girasole (leggermente di più rispetto a quello di oliva, le patate si devono un pelo immergere) per quelle fritte.<br><br> <strong>Per le patate:</strong> mettere le patate in pentola a fuoco lento, facendo attenzione agli eventuali schizzi dell'olio caldo con l'acqua contenuta nelle patate surgelate (l'acqua nell'olio bollente fa DANNI!)<br><br><strong>Per la carne:</strong> mettere l'hamburger in padella a fuoco basso e girarlo ogni tanto per non farlo bruciare. ",
         confirmButtonText: 'Capito!',
         customClass: {
             content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
@@ -32,10 +35,24 @@ function popupCheesecake(){
     });
 }
 
-function popupCoff(){
+function popupFrittata(){
     Swal.fire({
-        title: '<p class="swalfont">Caffè</p>',
-        html: "<img src='./img/icon.jpeg'></img><br><br><p class='swalfont'><strong>PREPARAZIONE</strong><br>Inserire acqua potabile nella parte sottostante della moka, SENZA superare la valvola.<br><br> Inserire il filtrino e mettere il caffè senza pressarlo, chiudere il tutto.<br><br>Mettere su un fornello a fiamma bassa e nel giro di pochi minuti avrai il tuo caffè!<br><br><strong>Per la pulizia:</strong> rimuovere tutto il caffè sotto acqua calda corrente. Usare solo spugnette, SENZA sapone per piatti!<br><br><iframe width='300' height='250' src='https://www.youtube.com/embed/yTMW9QWSvAE?si=cnmlA55oj372FWxe&amp;controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe></p>",
+        title: '<p class="swalfont">Frittata wurstel e formaggio 🍳</p>',
+        html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><br>Riscaldare una padella con un filo d'olio.<br><br>In una ciotola sbattere l'uovo unendo il parmigiano (50gxuovo) e un pizzico di sale.<br><br>Versare il composto nella padella calda e compattare la frittata, cuocere da entrambi i lati a fiamma bassa fino a doratura.",
+        confirmButtonText: 'Capito!',
+        customClass: {
+            content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
+        },
+    });
+}
+
+//dolci
+
+function popupCheesecake(){
+    Swal.fire({
+        title: '<p class="swalfont">Cheesecake 🍰</p>',
+        html: "<img src='./img/icon.jpeg'></img><br><br><strong>INGREDIENTI</strong><br><strong>Per la base:</strong><br>250 g di biscotti tipo digestive<br>125 g di burro.<br><strong>Per il ripieno:</strong><br> 750 g di Philadelphia<br>170 ml di panna fresca<br>80 g di zucchero<br>8 g di gelatina alimentare in fogli<br><strong>Per la copertura:</strong><br>250 g di confettura di lamponi<br>qualche goccia di succo di limone<br>mezzo bicchiere di acqua<br><br><strong>PREPARAZIONE</strong><br><strong>Per la base:</strong> spaccare i biscotti fino ad ottenere una polvere fine e mescolare col burro fuso. Livellare nello stampo e mettere in frigo in attesa del resto.<br><br><strong>Per il ripieno:</strong> mettere in ammollo la gelatina in acqua fredda per il tempo indicato sulla confezione, circa 10 minuti.<br>In una ciotola montare il formaggio fresco con lo zucchero a velo e 150 g di panna, fino a ottenere un composto omogeneo. Porre la panna restante in un pentolino e fatela scaldare.<br><br> Unire la gelatina ben strizzata alla panna calda e mescolare fino a quando si sarà sciolta. Far raffreddare e aggiungere il miscuglio al composto di formaggio fresco e mescolare.<br> Versare il tutto sulla base di biscotti, livellare e porre in frigorifero per un paio d'ore (più ci sta, più si solidifica per bene).<br><br><strong>Per la GANESH:</strong> scaldare la marmellata di lamponi in un pentolino con l'acqua e il succo di limone. Una volta tiepida, versarla sulla superficie del dolce. Porre nuovamente in frigorifero per un paio d'ore.",
+        //html: "<img src='./img/icon.jpeg'></img><br><br><strong>PREPARAZIONE</strong><ol><li>Iniziate preparando la base per il cheesecake.</li><li>Tritate finemente i biscotti secchi di vostra scelta (solitamente si usano biscotti tipo digestive o oreo).</li><li>In una ciotola, mescolate i biscotti tritati con del burro fuso fino a ottenere una consistenza simile a sabbia umida.</li><li>Pressate questa miscela di biscotti sul fondo di una teglia a cerniera precedentemente foderata con carta forno. Assicuratevi di compattarla bene.</li><li>Mettete la base in frigorifero mentre preparate il ripieno.</li></ol><strong>Per il ripieno:</strong><ol><li>In una ciotola capiente, sbattete il formaggio cremoso (tipo Philadelphia) fino a renderlo liscio e cremoso.</li><li>Aggiungete lo zucchero a velo e continuate a mescolare fino a ottenere una consistenza omogenea.</li><li>Aggiungete la vaniglia e il succo di limone e mescolate bene.</li><li>In un'altra ciotola, montate la panna fresca fino a ottenere picchi morbidi.</li><li>Incorporate delicatamente la panna montata al composto di formaggio, mescolando con movimenti delicati dal basso verso l'alto per non smontare la panna.</li><li>Versate il ripieno sulla base di biscotti nella teglia.</li><li>Livellate la superficie con una spatola e coprite la teglia con pellicola trasparente.</li><li>Mettete il cheesecake in frigorifero e lasciatelo raffreddare per almeno 4 ore o preferibilmente durante la notte.</li></ol><strong>Per la guarnizione:</strong><ol><li>Prima di servire, decorate il cheesecake con frutti di bosco freschi o sciroppo di frutta, se desiderate.</li><li>Rimuovete con attenzione il cheesecake dalla teglia a cerniera e trasferitelo su un piatto da portata.</li><li>Tagliate a fette e gustate il vostro delizioso cheesecake freddo!</li></ol>",
         confirmButtonText: 'Capito!',
         customClass: {
             content: 'swalfont', // Classe CSS personalizzata per il contenuto dell'alert
